e655973ab199a458af39ea6928503c82adb993c6
HussainAlii <husain_sa2@hotmail.com>
2021-03-27 19:40:04 +0300

    quick login page

--- app/src/main/java/com/example/therecipesbank/MainActivity.java
+++ app/src/main/java/com/example/therecipesbank/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.therecipesbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String username = "admin";
+    private static final String password = "123";
+    EditText usernameInput, passwordInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.username);
+        passwordInput = findViewById(R.id.passwordInput);
+    }
+
+    public void login(View view){
+        if(usernameInput.getText().equals("admin")  && passwordInput.getText().equals("123")){
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
