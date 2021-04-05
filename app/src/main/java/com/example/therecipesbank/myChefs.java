d017ccc0687454d588977cd8d0cb7ab0056a43ce
HussainAlii <husain_sa2@hotmail.com>
2021-04-05 22:11:34 +0300

    yay

--- app/src/main/java/com/example/therecipesbank/myChefs.java
+++ app/src/main/java/com/example/therecipesbank/myChefs.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
  */
 public class myChefs extends Fragment {
 
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -48,6 +49,22 @@ public class myChefs extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_chefs, container, false);
     }
 
 }
