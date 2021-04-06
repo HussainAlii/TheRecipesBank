717b3a65620201854d6b758bb7a6f28f96afc95b
CLOUD314 <49446265+CLOUD314@users.noreply.github.com>
2021-04-07 00:21:50 +0300

    recipe view

---
+++ app/src/main/java/com/example/therecipesbank/recipeView.java
@@ -0,0 +1,14 @@
+package com.example.therecipesbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recipeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_view);
+    }
+}
