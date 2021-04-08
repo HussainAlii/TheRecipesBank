2fde251c7629e3e06b9c4654aec0a50d9c8801a9
Abdelmonem <fgithubonly@hotmail.com>
2021-04-08 21:23:35 +0300

    |

--- app/src/main/java/com/example/therecipesbank/Popular.java
+++ app/src/main/java/com/example/therecipesbank/Popular.java
@@ -83,18 +83,18 @@ public class Popular extends Fragment {
         Button myChefsButton = view.findViewById(R.id.popular_myChefsButton);
         Button trendsButton = view.findViewById(R.id.popular_trendsButton);
 
-        String foodList []= {"Apple","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate"};
-
-        ListAdapter theAdapter = new myAdapter(getContext(), foodList);
-        ListView trendList = view.findViewById(R.id.trendsList);
-        trendList.setAdapter(theAdapter);
-
-        trendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                //adapterView.getItemAtPosition(position)
-            }
-        });
+//        String foodList []= {"Apple","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate","Chocolate"};
+//
+//        ListAdapter theAdapter = new myAdapter(getContext(), foodList);
+//        ListView trendList = view.findViewById(R.id.trendsList);
+//        trendList.setAdapter(theAdapter);
+//
+//        trendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                //adapterView.getItemAtPosition(position)
+//            }
+//        });
 
         final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
 
