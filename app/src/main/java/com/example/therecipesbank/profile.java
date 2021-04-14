30d600c22fb147cbf0c175b40fac13520dd356ca
HussainAlii <husain_sa2@hotmail.com>
2021-04-14 19:02:53 +0300

    y

--- app/src/main/java/com/example/therecipesbank/profile.java
+++ app/src/main/java/com/example/therecipesbank/profile.java
@@ -86,6 +86,7 @@ public class profile extends Fragment {
         Button trendsButton          = view.findViewById(R.id.profile_trendsButton);
         Button confirmButton         = view.findViewById(R.id.update_button);
         Button signOutButton         = view.findViewById(R.id.sigOutButton);
+        Button myDishesButton         = view.findViewById(R.id.myDishes_button);
 
 
 
@@ -154,6 +155,13 @@ public class profile extends Fragment {
                 //set new profile info here from the database
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        myDishesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_profile_to_myPosts);
             }
         });
     }
